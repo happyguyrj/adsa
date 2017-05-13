bd3549b6e74422baffff8c5e6e92b028c5e123f5
happyguyrj <jainrahuldn@gmail.com>
2017-05-13 16:51:42 +0530

    seqlinearlist lib complete

--- library/seqlinearlist.hpp
+++ library/seqlinearlist.hpp
@@ -70,62 +70,99 @@ class LinearList{
 
 template<class Item>
   LinearList<Item>::LinearList(){
-
+		element = new Item;
   }
 
 template<class Item>
   LinearList<Item>::LinearList(const int& MaxListSize){
-
+		MaxSize = MaxListSize;
+		element = new Item[MaxListSize];
+		for (int i = 0; i < MaxSize; i++) {
+			element[i]  = 0;
+		}
   }
 
 template<class Item>
   LinearList<Item>::~LinearList(){
+		delete element;
+  }
+
+template<class Item>
+  Item& LinearList<Item>::operator[](const int& i){
 
   }
 
 template<class Item>
-  LinearList<Item>::operator[](const int& i){
-
+  bool LinearList<Item>::isEmpty(){
+		return (len == 0);
   }
 
 template<class Item>
-  LinearList<Item>::isEmpty(){
-
-  }
-
-template<class Item>
-  LinearList<Item>::length(){
-
+  int LinearList<Item>::length(){
+		return len;
   }
 
 template<class Item>
   LinearList<Item>::maxSize(){
-
+		return maxSize;
   }
 
 template<class Item>
-  LinearList<Item>::returnListElement(const int k){
-
+  int LinearList<Item>::returnListElement(const int k){
+		return element[k];
   }
 
 template<class Item>
-  LinearList<Item>::find(const int k, Item& x){
-
+  bool LinearList<Item>::find(const int k, Item& x){
+		if(k < MaxSize){
+			if (x == element[k-1]) {
+				return true;
+			}
+		}
+		else{
+			cout <<"Index out of bound"<< endl;
+			return false;
+		}
   }
 
 template<class Item>
-  LinearList<Item>::search(Item& x){
-
+  int LinearList<Item>::search(Item& x){
+		if(k < MaxSize){
+			if (x == element[k-1]) {
+				return i;
+			}
+		}
+		else{
+			cout <<"Index out of bound"<< endl;
+			return false;
+		}
   }
 
 template<class Item>
-  LinearList<Item>::insert(const int k, Item& x){
+  void LinearList<Item>::deleteElement(const int  k, Item& x){
+		x = element[k];
+		for(int i=k+1;i<len;i++){
+			element[i-1] = element[i];
+		}
+		len--;
+}
 
+template<class Item>
+  void LinearList<Item>::insert(const int k, Item& x){
+		for (int i = k+1; i < MaxSize; i++) {
+			element[i+1] = element[i];
+		}
+		element[k+1] = x;
+		len++;
   }
 
 template<class Item>
   LinearList<Item>::display(){
-
+		cout << "Elements are: ";
+		for (int i = 0; i < len; i++) {
+			cout << element[i] << " -";
+		}
+		cout << endl;
   }
 
 #endif
