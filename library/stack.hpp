f48f1520f98f2dad4a535e4a0dc858a93e02e2d4
happyguyrj <jainrahuldn@gmail.com>
2017-05-15 11:13:38 +0530

    stack lib com

--- library/stack.hpp
+++ library/stack.hpp
@@ -15,44 +15,67 @@
 
 #ifndef STACK_HPP_
 #define STACK_HPP_
-namespace /*Your namespace here*/
-{
-  template<typename T>
-    class stack
-    {
-      public:
-        /*
-         * Constructs a new stack.
-         */
-        stack();
-        /*
-         * Pushes t to on the top of the stack.
-         */
-        void push(const T& t);
-        /*
-         * Returns the element at the top of the stack.
-         * Also removes the top element from the stack.
-         */
-        T pop();
-        /*
-         * Returns the element at the top of the stack.
-         * Does not remove the top element.
-         */
-        T top();
-        /*
-         * Returns the number of elements currently in the stack.
-         */
-        int size();
-        /*
-         * Returns a boolean indicating whether the stack is empty or not.
-         */
-        inline bool empty();
-        /*
-         * Destructor
-         * Fress the memory occupied by the stack elements.
-         */
-        ~stack();
-    };
-}
+
+#include "list.hpp"
+
+using namespace std;
+
+template<typename T>
+  class stack
+  {
+    list<T> s;
+
+    public:
+      /*
+       * Constructs a new stack.
+       */
+      stack(){}
+      /*
+       * Pushes t to on the top of the stack.
+       */
+      void push(const T& t){
+        s.cons(t);
+      }
+      /*
+       * Returns the element at the top of the stack.
+       * Also removes the top element from the stack.
+       */
+      T pop(){
+        Node<T> * head = s.gethead();
+        T h = head->getdata();
+        s.remove(h);
+        return h;
+      }
+      /*
+       * Returns the element at the top of the stack.
+       * Does not remove the top element.
+       */
+      T top(){
+        Node<T> * head = s.gethead();
+        return head->getdata();
+      }
+      /*
+       * Returns the number of elements currently in the stack.
+       */
+      int size(){
+        return s.length();
+      }
+      /*
+       * Returns a boolean indicating whether the stack is empty or not.
+       */
+      inline bool empty(){
+        return s.empty();
+      }
+      /*
+       * Destructor
+       * Fress the memory occupied by the stack elements.
+       */
+      ~stack(){}
+
+      void display(){
+        s.print();
+      }
+  };
+
 
 #endif
